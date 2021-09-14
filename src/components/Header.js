1e66c8365c12118da7b5d37669c5a9c81160866e
Alexandre-st <alexandre.saintprix@gmail.com>
2021-09-14 18:25:51 +0200

    Can add the switch change for the theme but not the background

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import moon from '../assets/images/icon-moon.svg';
+import sun from '../assets/images/icon-sun.svg';
+import loupe from '../assets/images/icon-search.svg';
 
 const Header = () => {
+  // const setDarkMode = () => {
+  //   document.querySelector('body').classList = 'dark';
+  //   localStorage.setItem('colorMode', 'dark');
+  // };
+
+  // const setLightMode = () => {
+  //   document.querySelector('body').classList = 'light';
+  //   localStorage.setItem('colorMode', 'light');
+  // }
+
+  const [mode, setMode] = useState('light');
+
+  const onSelectMode = (mode) => {
+    setMode(mode);
+    if (mode === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
+
   return ( 
     <header>
-      <div>
+      <div className="header">
         <h1>devfinder</h1>
-        <p>toggle switch</p>
+        {/* <div className="header-toggle" onClick={onSelectMode}> */}
+          {mode === 'dark' ? 
+            <div className="header-toggle" onClick={() => onSelectMode('light')}>
+              <p>LIGHT</p> 
+              <img src={sun} alt="Soleil pour changer la couleur du thème" />
+            </div>
+            : 
+            <div className="header-toggle" onClick={() => onSelectMode('dark')}>
+              <p>DARK</p> 
+              <img src={moon} alt="Lune pour changer la couleur du thème" />
+            </div>
+          }
+        {/* </div> */}
+
+      </div>
+      <div className="search">
+        <form className="search-bar" id="searchForm">
+          <div className="search-bar-glass">
+            <img src={loupe} alt="Loupe de recherche  " />
+            <input type="text" id="username" placeholder="Search GitHub username…" />
+          </div>
+          <button type="submit">Search</button>
+        </form>
       </div>
     </header>
    );
