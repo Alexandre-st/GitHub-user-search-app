3cb72e6f7aff2a5c9992f1efeaedfa6e48d91285
Alexandre-st <alexandre.saintprix@gmail.com>
2021-10-07 23:09:08 +0200

    Add validation propTypes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,4 @@
+import propTypes from 'prop-types';
 import loupe from '../assets/images/icon-search.svg';
 
 const SearchBar = ({ setUsername, fetchUser }) => {
@@ -6,28 +7,34 @@ const SearchBar = ({ setUsername, fetchUser }) => {
     <section>
       <div className="search">
         <div className="search-bar">
-      
-              <div className="search-bar-glass">
-                <img src={loupe} alt="Loupe de recherche" />
-                <label htmlFor="search-header">
-                  <span className="visually-hidden">
-                    Search GitHub username…
-                  </span>
-                </label>
-                <input 
-                  id="search-header"
-                  type="text" 
-                  autoComplete="off"
-                  placeholder="Search GitHub username…" 
-                  onChange={(evt) => setUsername(evt.target.value)}
-                />
-              </div>
-              <span className="error">No results</span>
-              <button onClick={() => fetchUser()}>Search</button>
+          <div className="search-bar-glass">
+            <img src={loupe} alt="Loupe de recherche" />
+            <label htmlFor="search-header">
+              <span className="visually-hidden">
+                Search GitHub username…
+              </span>
+            </label>
+            <input 
+              id="search-header"
+              type="text" 
+              autoComplete="off"
+              placeholder="Search GitHub username…" 
+              onChange={(evt) => setUsername(evt.target.value)}
+            />
           </div>
+          <span className="error">No results</span>
+          <button onClick={() => fetchUser()}>
+            Search
+          </button>
+        </div>
       </div>
     </section>
   );
-}
+};
+
+SearchBar.propTypes = {
+  setUsername: propTypes.string,
+  fetchUser: propTypes.func
+};
  
 export default SearchBar;
